aa636164a8123e58579e9481ba965d57e48c7d37
shadabhashmi21 <er.shadabhashmi@gmail.com>
2023-06-03 15:31:55 +0530

    added: appbar text

--- app/src/main/java/com/example/hyperhireassignment/view/PostActivity.java
+++ app/src/main/java/com/example/hyperhireassignment/view/PostActivity.java
@@ -1,16 +1,27 @@
 package com.example.hyperhireassignment.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.hyperhireassignment.R;
 
 public class PostActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
     }
 }
